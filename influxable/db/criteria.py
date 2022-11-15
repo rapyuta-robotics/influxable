cd3a9b8831600f1c40161deef582d73da75eb239
Swagnik Dutta <swagnikd@gmail.com>
2022-11-15 18:44:56 +0530

    fixed auto correction issue

--- influxable/db/criteria.py
+++ influxable/db/criteria.py
@@ -89,8 +89,8 @@ class Criteria:
         operator = EVALUATED_OPERATORS[self.operator]
         right_operand = self.right_operand
         if isinstance(right_operand, str):
-            if operator in [EVALUATED_OPERATORS[WhereOperatorEnum.REGEQ], EVALUATED_OPERATORS[WhereOperatorEnum.REGNEQ]]
-                    or operator == EVALUATED_OPERATORS[WhereOperatorEnum.REGNEQ]:
+            if operator in [EVALUATED_OPERATORS[WhereOperatorEnum.REGEQ],
+                            EVALUATED_OPERATORS[WhereOperatorEnum.REGNEQ]]:
                 right_operand = '/{}/'.format(self.right_operand)
             else:
                 right_operand = '\'{}\''.format(self.right_operand)
